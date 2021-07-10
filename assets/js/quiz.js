e841f5b3367144b2f3d1305162523ed3b50d2ab8
J0hn1975 <johnwrafter@live.co.uk>
2021-07-10 15:24:01 +0000

    Add quiz.js file

---
+++ assets/js/quiz.js
@@ -0,0 +1,44 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+function buildQuiz(){}
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
+
+const myQuestions = [
+    {
+      question: "What is the captial of Santorini?",
+      answers: {
+        a: "Fira",
+        b: "Oia",
+        c: "Imerovigli"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
