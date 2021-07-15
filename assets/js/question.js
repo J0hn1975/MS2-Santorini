c7639ec9713c1c730ae3d98326b55d31d2d5b36c
J0hn1975 <johnwrafter@live.co.uk>
2021-07-15 20:42:20 +0000

    JS: Add comments to number questions

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,51 +1,61 @@
 // Array of Objects
 const quiz = [
   {
+    // Question 1
   	q:'What is the capital of Santorini?',
   	options:['Fira','Oia','Ancient Thera','Imerovigli'],
   	answer:0
   },
   {
+    // Question 2
   	q:'Where can you see the famous blue domes?',
   	options:['Fira','Kamari','Oia','Firostefani'],
   	answer:2
   },
   {
+    // Question 3
   	q:'There are more churches than houses?',
   	options:['true','false'],
   	answer:0
   },
   {
+    // Question 4
   	q:'Where is the best place on the island to see the sunsets?',
   	options:['Fira','Pyrgos','Emporoi','Oia'],
   	answer:3
   },
   {
+    // Question 5
   	q:'Santorini is built on the remains of a volcanic caldera?',
   	options:['True','False'],
   	answer:2
   },
   {
+    // Question 6
     q:"'What part of the island is known as Santorini's balcony?'",
     options:['Imerovigli','Oia','Kamari','Fira'],
     answer:0,
   },
   {
+    // Question 7
     q:'Santorini is named after Saint Irene?',
     options:['True','False',],
     answer:0,
   },
   {
+    // Question 8
     q:'Windows, macOS, and Linux are examples of ________.?',
     options:['web browsers','mobile devices','filmy heroines','operating systems'],
     answer:3,
   },
   {
+    // Question 9
     q:'Santorini is located in which sea?',
     options:['Mediterranean','Ionian Sea','Aegean Sea'],
     answer:2,
   },
   {
+    // Question 10
     q:'The best in Greece can be found on Santorini?',
     options:['True','False',],
     answer:1,
